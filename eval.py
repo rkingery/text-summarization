b8eb1f7d71134d645026421a89d43c1e2cf844ff
rkingery <rkinger@g.clemson.edu>
2018-11-28 17:13:21 -0500

    added eval.py

---
+++ eval.py
@@ -0,0 +1,108 @@
+import argparse
+from pythonrouge.pythonrouge import Pythonrouge
+import os
+
+
+def rouge_para(predict_path, golden_path):
+
+    # initialize setting of ROUGE, eval ROUGE-1, 2, SU4
+    # if summary_file_exis=True, you should specify predict summary(peer_path) and golden summary(model_path) paths
+    rouge = Pythonrouge(summary_file_exist=True,
+                        peer_path=predict_path, model_path=golden_path,
+                        n_gram=2, ROUGE_SU4=True, ROUGE_L=True,
+                        recall_only=True,
+                        stemming=True, stopwords=True,
+                        word_level=True, length_limit=True, length=50,
+                        use_cf=False, cf=95, scoring_formula='average',
+                        resampling=True, samples=1000, favor=True, p=0.5)
+
+    score = rouge.calc_score()
+    print(score)
+
+
+def rouge_sent(predict_path, golden_path):
+
+    import spacy
+    nlp = spacy.load('en_core_web_sm')
+
+    with open(predict_path, 'r') as rf:
+        predict_summary = rf.read().replace('\n', ' ')
+    with open(golden_path, 'r') as rf:
+        golden_summary = rf.read().replace('\n', ' ')
+
+    doc = nlp(predict_summary)
+    predict_sent_list = [sent.text for sent in doc.sents]
+
+    doc = nlp(golden_summary)
+    golden_sent_list = [sent.text for sent in doc.sents]
+
+    max_rouge_1 = -99999
+    max_rouge_2 = -99999
+
+    for p_idx, predict_sent in enumerate(predict_sent_list):
+        for g_idx, golden_sent in enumerate(golden_sent_list):
+
+            # initialize setting of ROUGE to eval ROUGE-1, 2, SU4
+            # if you evaluate ROUGE by sentence list as above, set summary_file_exist=False
+            # if recall_only=True, you can get recall scores of ROUGE
+            rouge = Pythonrouge(summary_file_exist=False,
+                                summary=[[predict_sent]], reference=[[[golden_sent]]],
+                                n_gram=2, ROUGE_SU4=True, ROUGE_L=True,
+                                recall_only=True, stemming=True, stopwords=True,
+                                word_level=True, length_limit=True, length=50,
+                                use_cf=False, cf=95, scoring_formula='average',
+                                resampling=True, samples=1000, favor=True, p=0.5)
+            score = rouge.calc_score()
+
+            if score['ROUGE-1'] > max_rouge_1:
+                max_rouge_1 = score['ROUGE-1']
+                p_idx_rouge_1_save = p_idx
+                g_idx_rouge_1_save = g_idx
+
+            if score['ROUGE-2'] > max_rouge_2:
+                max_rouge_2 = score['ROUGE-2']
+                p_idx_rouge_2_save = p_idx
+                g_idx_rouge_2_save = g_idx
+
+    print("Max ROUGE-1 score among sentences: %.5f" % max_rouge_1)
+    print("[Predicted Sentence]\n%s" % predict_sent_list[p_idx_rouge_1_save])
+    print("[Golden Sentence]\n%s" % golden_sent_list[g_idx_rouge_1_save])
+
+    print()
+
+    print("Max ROUGE-2 score among sentences: %.5f" % max_rouge_2)
+    print("[Predicted Sentence]\n%s" % predict_sent_list[p_idx_rouge_2_save])
+    print("[Golden Sentence]\n%s" % golden_sent_list[g_idx_rouge_2_save])
+
+
+def cov_entity(predict_path, golden_path):
+
+    import spacy
+    nlp = spacy.load('en_core_web_sm')
+
+    with open(predict_path, 'r') as rf:
+        predict_summary = rf.read().replace('\n', ' ')
+    with open(golden_path, 'r') as rf:
+        golden_summary = rf.read().replace('\n', ' ')
+
+    doc = nlp(predict_summary)
+    predict_entity_list = list(set([ent.text for ent in doc.ents]))
+
+    doc = nlp(golden_summary)
+    golden_entity_list = list(set([ent.text for ent in doc.ents]))
+
+    def intersection(lst1, lst2):
+        return list(set(lst1) & set(lst2))
+
+    coverage = len(intersection(predict_entity_list, golden_entity_list)) / len(golden_entity_list)
+
+    print('Entity Coverage: %.2f%%' % (coverage * 100))
+    print("[Predicted]")
+    print(predict_entity_list)
+    print("[Golden]")
+    print(golden_entity_list)
+
+
+if __name__ == "__main__":
+    golden_path = os.getcwd()+'/golden/'
+    predict_path = os.getcwd()+'/predict/'
